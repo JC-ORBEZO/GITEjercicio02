fa5f80dab4250139740681f790c8b1b45131335a
JC-ORBEZO <jose.orbezo@alumnos.frgp.utn.edu.ar>
2022-02-15 14:09:12 -0300

    yubu yubi

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ console.log("Bienvenido a bordo");
 //console.log("Segundo clg de prueba");
 const variable="esta es una variable pa";
 console.log(variable);
+for(let i=0; i<10; i++){
+    console.log("num " + i);
+}
